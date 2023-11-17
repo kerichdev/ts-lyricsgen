3399f027d91bbe339bb5814ddeab6dd76264a3fa
kerichdev <kerichuudev@gmail.com>
2023-11-17 17:35:32 +0300

    [add] some necessary imports from mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,21 @@
-import { useState } from 'react'
+import {
+  Button,
+  Select,
+  TextField,
+  Chip,
+  List,
+  Tooltip,
+  Alert,
+  Snackbar
+} from "@mui/material"
+
+import {
+  Box,
+  Container,
+  Stack
+} from "@mui/system"
+
+import * as React from 'react';
 import './App.css'
 
 function App() {
