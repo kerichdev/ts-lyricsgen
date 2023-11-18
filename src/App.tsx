077744aaa8d73a2886d00759d108d0354e49851c
kerichdev <kerichuudev@gmail.com>
2023-11-18 18:24:03 +0300

    [add] test request, seems to be working

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,35 @@ import {
   Alert,
   Snackbar
 } from "@mui/material"
-
+//
 import {
   Box,
   Container,
   Stack
 } from "@mui/system"
-
-import * as React from 'react';
+//
+import { useQuery } from "react-query";
+import searchQuery from "./api/searchQuery";
+//
+import React, { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
+
+  const track = "sovereign";
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ['findTrack', track],
+    queryFn: async () => {
+     const data = await searchQuery(track);
+     return data.data.results;
+    }
+   });
+    
+  useEffect(() => {
+    if (!isLoading) {
+    console.log(isError ? error : data);
+    }
+   }, [isLoading]);
 
   return (
     <>
