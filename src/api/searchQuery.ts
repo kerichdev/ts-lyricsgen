8ef8162b2a4b95d15ed8ea27fc3c70b1d66ed696
kerichdev <kerichuudev@gmail.com>
2023-11-18 17:51:51 +0300

    [add] api key present check

--- src/api/searchQuery.ts
+++ src/api/searchQuery.ts
@@ -1,8 +1,22 @@
 import axios from 'axios';
 import { Results } from "./lastfmSearch.interfaces";
 
-export default async function searchQuery(track: string, api_key: string) {
+async function LoadKey() {
+  try {
+    const secrets = await import("../secrets.ts").catch(err => err);
+    if (secrets.API_KEY) {
+      return secrets.API_KEY;
+    }
+  }
+  catch {
+    return "NO_API_KEY_IDIOT";
+  }
+}
+
+const key = LoadKey();
+
+export default async function searchQuery(track: string) {
   return await axios.get<Results>(
-    `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${track}&api_key=${api_key}&format=json`
+    `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${track}&api_key=${key}&format=json`
   );
 }
