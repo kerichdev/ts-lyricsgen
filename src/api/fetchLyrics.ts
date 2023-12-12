f8666d5641c8229d3fffb591249694427423a92b
kerichdev <kerichuudev@gmail.com>
2023-12-12 14:49:22 +0300

    [fix] update function name to not conflict with lastfm

--- src/api/fetchLyrics.ts
+++ src/api/fetchLyrics.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Lyrics } from "./lyrist.interface";
 
-export default async function searchQuery(track: string, artist: string) {
+export default async function lyristQuery(track: string, artist: string) {
   return await axios.get<Lyrics>(
     `https://lyrist.vercel.app/api/${track}/${artist}`
   );
