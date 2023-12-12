0fd0a2121a3097afbf9dcedea542cdfe88675958
kerichdev <kerichuudev@gmail.com>
2023-12-12 14:08:31 +0300

    [add] working api request on click - amazing!

--- src/App.tsx
+++ src/App.tsx
@@ -14,40 +14,42 @@ import {
   Container,
   Stack
 } from "@mui/material"
-//
 import { useQuery } from "react-query";
 import searchQuery from "./api/searchQuery";
-//
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ReactNode } from 'react';
 import './App.css'
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const { isLoading, data, isError, error, refetch } = useQuery(['search', searchText], () => searchQuery(searchText), {
+    enabled: false, // dont query automatically
+  });
 
-  const track = "sovereign";
-  const { isLoading, data, isError, error } = useQuery({
-    queryKey: ['findTrack', track],
-    queryFn: async () => {
-     const data = await searchQuery(track);
-     return data.data.results;
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleSearch = async () => {
+    try {
+      await refetch();
+    } catch (error) {
+      console.error(error);
     }
-   });
-    
-  useEffect(() => {
-    if (!isLoading) {
-    console.log(isError ? error : data);
-    }
-   }, [isLoading]);
+  };
 
   return (
     <>
       <h1> Well... this is awkward. </h1>
       <p> Search for your song below: </p>
-      <TextField 
-        fullWidth 
-        id="search-field" 
-        label="Search" 
-        variant="filled"
+      <TextField
+        label="Enter your search query"
+        value={searchText}
+        onChange={handleSearchChange}
       />
+      <Button onClick={handleSearch}>Search</Button>
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error: {String(error)}</p>}
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </>
   )
 }
