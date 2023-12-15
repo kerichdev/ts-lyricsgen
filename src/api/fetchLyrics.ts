d8e2a06845395bc60b5e45cfe6da5e44611a7268
kerichdev <kerichuudev@gmail.com>
2023-12-15 12:22:31 +0300

    [broken] attempt to fix the api not working

--- src/api/fetchLyrics.ts
+++ src/api/fetchLyrics.ts
@@ -3,6 +3,11 @@ import { Lyrics } from "./lyrist.interface";
 
 export default async function lyristQuery(track: string, artist: string) {
   return await axios.get<Lyrics>(
-    `https://lyrist.vercel.app/api/${track}/${artist}`
+    `https://lyrist.vercel.app/api/${track}/${artist}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "Origin": "https://lyrist.vercel.app"
+      },
+    }
   );
 }
