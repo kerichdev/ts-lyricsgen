3c39838bc1fa596daa8b1b4d3c4cd3368ec6350f
kerichdev <kerichuudev@gmail.com>
2024-01-10 12:54:48 +0300

    [add] use env var for api key

--- src/api/searchQuery.ts
+++ src/api/searchQuery.ts
@@ -1,20 +1,11 @@
 import axios from 'axios';
 import { Results } from "./lastfmSearch.interfaces";
 
-async function loadKey() {
-  try {
-    const secrets = await import("../secrets.ts").catch(err => err);
-    if (secrets.API_KEY) {
-      return secrets.API_KEY;
-    }
-  }
-  catch {
-    return "NO_API_KEY_IDIOT";
-  }
-}
-
 export default async function searchQuery(track: string) {
-  const key = await loadKey();
+  const key = import.meta.env.VITE_LASTFM;
+  if (!key || key === '') {
+    throw new Error('Get a Last.fm API key, idiot');
+  }
   return await axios.get<Results>(
     `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${track}&api_key=${key}&format=json`
   );
