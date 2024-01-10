fae4cc7d766186814b63ac55ae64d9eb95266c6c
kerichdev <kerichuudev@gmail.com>
2024-01-10 12:52:31 +0300

    [fix] dont call api if empty request

--- src/api/fetchLyrics.ts
+++ src/api/fetchLyrics.ts
@@ -2,8 +2,10 @@ import axios from 'axios';
 import { Lyrics } from "./lyrist.interface";
 
 export default async function lyristQuery(track: string, artist: string) {
+  if(!track || !artist) return;
+  
   return await axios.get<Lyrics>(
-    `https://lyrist.vercel.app/api/${track}/${artist}`, {
+    `https://lyrist.vercel.app/api/${encodeURIComponent(artist)}/${encodeURIComponent(track)}`, {
       headers: {
         "Content-Type": "application/json",
       },
