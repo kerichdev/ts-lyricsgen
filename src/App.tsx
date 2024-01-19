44287d0d0f792a8d97a79a08b8e93c0396697384
kerichdev <kerichuudev@gmail.com>
2024-01-19 14:25:17 +0300

    [add] modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import SearchTrack from "./components/SearchTrack";
 import DisplayLyrics from "./components/DisplayLyrics";
 import './index.css'
+import { useState } from "react";
+import LyricsModal from "./components/LyricsModal";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleOpen = () => setIsModalOpen(true);
+  const handleClose = () => setIsModalOpen(false);
+
   return (
     <>
       <h1> Spotify lyrics card maker </h1>
       <p> Search for your song below: </p>
       <div className="displayClass">
         <SearchTrack />
-        <DisplayLyrics />
+        <DisplayLyrics handleOpen={handleOpen} />
       </div>
+      <LyricsModal handleClose={handleClose} isOpen={isModalOpen} />
     </>
   )
 }
