7ac84f11b16006e63f82c667cbc1bae31ac3a74f
kerichdev <kerichuudev@gmail.com>
2024-01-19 11:49:46 +0300

    [test] export card on click

--- src/components/ColoredCard.tsx
+++ src/components/ColoredCard.tsx
@@ -1,5 +1,7 @@
 import { useColor } from "color-thief-react";
 import { Backdrop, CircularProgress } from "@mui/material";
+import { CSSProperties, useRef } from "react";
+import exportAsImage from "../utils/exportAsImage";
 
 interface Props {
   title: string;
@@ -8,14 +10,22 @@ interface Props {
   lyrics: string;
 }
 
+interface CardStyles extends CSSProperties {
+  "--bg": string;
+}
+
 const ColoredCard = (props: Props) => {
   const { title, artist, imgLink, lyrics } = props;
+
+  const exportRef = useRef<HTMLDivElement | null>(null);
 
   const googleProxyURL =
     "https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=";
 
+  const finalImageURL = googleProxyURL + encodeURIComponent(imgLink);
+
   const { data, loading, error } = useColor(
-    googleProxyURL + encodeURIComponent(imgLink),
+    finalImageURL,
     "hslArray",
     { crossOrigin: "anonymous" }
   );
@@ -29,30 +39,30 @@ const ColoredCard = (props: Props) => {
   }
 
   if (error) {
-    throw new Error(error);
+    return <div className="errorCard">Something went wrong...</div>
   }
 
   if (data) {
-    console.log(data);
     return (
       <div
         className="column"
         id="lyrics"
         style={{
-          backgroundColor: `hsl(${data[0]}, ${data[1]}%, 
-                                                        ${parseInt(data[2]) < 40 ? 40 :
-                                                          parseInt(data[2]) > 80 ? 80 : data[2]}%)`}}
+          "--bg": `hsl(${data[0]}, ${data[1]}%, 
+            ${parseInt(data[2]) < 40 ? 40 :
+              parseInt(data[2]) > 80 ? 80 : data[2]}%)`,} as CardStyles }
+        ref={exportRef}
       >
         <div className="lyricsCard">
-          <img className="albumCover" src={imgLink} />
+          <img className="albumCover" src={finalImageURL} />
           <div className="lyricsNames">
             <h2> {title} </h2>
             <h3> {artist} </h3>
           </div>
         </div>
         <div className="lyricsScrollable">
-          {lyrics.split("\n").map((line, index) => (
-            <p key={index}> {line} </p>
+          {lyrics && lyrics.split("\n").map((line, index) => (
+            <p key={index} onClick={() => exportAsImage(exportRef!.current, "test")}> {line} </p>
           ))}
         </div>
         <div className="copyrightDiv">
